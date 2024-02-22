44a0ecb30288987c1d0e3286530cd672c52a1760
Titus Omondi <101582094+titusomondi@users.noreply.github.com>
2024-02-22 09:12:08 +0300

    inital commit

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Ask for user's name
+name = input("Enter your name: ")
+
+# Ask for user's age
+age = input("Enter your age: ")
+
+# Ask for user's location
+location = input("Enter your location: ")
+
+# Print personalized message
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
